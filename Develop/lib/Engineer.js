44ca5bcfd81b816493716fb8b5392264c6fb4c36
Kathleen Tonner <kathleentonner@gmail.com>
2020-04-03 22:59:07 -0400

    Intern class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,4 +1,5 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
 
 class Engineer extends Employee{
     constructor(name, id, email, github, githubUser){
@@ -14,11 +15,3 @@ class Engineer extends Employee{
         return "Engineer"
     }
 }
-
-
-
-// * github  // GitHub username
-
-// * getGithub()
-
-// * getRole() // Overridden to return 'Engineer'
