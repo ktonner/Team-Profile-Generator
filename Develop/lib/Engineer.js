6e425340eddffaf50f2503112c2b60ee329f3d38
Kathleen Tonner <kathleentonner@gmail.com>
2020-04-03 22:56:21 -0400

    Engineer class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Engineer extends Employee{
+    constructor(name, id, email, github, githubUser){
+    super(name, id, email)
+    this.github = github;
+    this.githubUser = githubUser
+    }
+    getGithub = function(){
+        return this.githubUser
+        return this.github
+    }
+    getRole = function(){
+        return "Engineer"
+    }
+}
+
+
+
+// * github  // GitHub username
+
+// * getGithub()
+
+// * getRole() // Overridden to return 'Engineer'
